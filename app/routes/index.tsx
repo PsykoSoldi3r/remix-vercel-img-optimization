e4560198dd29d41e52a03548942e5da3eb9c7405
Joris <reixjoris@gmail.com>
2022-08-09 09:47:08 +0200

    chore: add title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,12 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <img
+      <h1>Remix + Vercel Image Optimization</h1>
+
+      {/* <img
         src="/_vercel/image?url=/img/remix.png&w=400&q=75"
         alt="Remix logo"
-      />
+      /> */}
     </div>
   );
 }
