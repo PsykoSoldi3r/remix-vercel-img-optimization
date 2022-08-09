ad66274ab914fc93c686f257239d6145c8f8b356
Joris <reixjoris@gmail.com>
2022-08-09 10:37:31 +0200

    ls

--- post-build.js
+++ post-build.js
@@ -1,7 +1,17 @@
-"use strict";
-
+//requiring path and fs modules
+const path = require("path");
 const fs = require("fs");
-
-let rawdata = fs.readFileSync(".vercel/output/config.json");
-let data = JSON.parse(rawdata);
-console.log(data);
+//joining path of directory
+const directoryPath = path.join(__dirname, ".");
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+  //handling error
+  if (err) {
+    return console.log("Unable to scan directory: " + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+    // Do whatever you want to do with the file
+    console.log(file);
+  });
+});
