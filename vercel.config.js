c41942d92139aee317407620340e39bd525319bc
Joris <reixjoris@gmail.com>
2022-08-09 10:49:03 +0200

    vercel.config.js

--- vercel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  version: 3,
-  images: {
-    domains: [],
-    sizes: [640, 750, 828, 1080, 1200],
-    minimumCacheTTL: 60,
-    formats: ["image/webp", "image/avif"],
-  },
-};
