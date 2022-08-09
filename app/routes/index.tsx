5171d18d5202e29ff02367e860c3a71eaa516e3a
Joris <reixjoris@gmail.com>
2022-08-09 10:01:54 +0200

    ing

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,11 +2,10 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Remix + Vercel Image Optimization</h1>
-
-      {/* <img
+      <img
         src="/_vercel/image?url=/img/remix.png&w=400&q=75"
         alt="Remix logo"
-      /> */}
+      />
     </div>
   );
 }
